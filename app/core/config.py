769778bd3cbf1a6d70c2d01482289129d74a8c1c
EranMani <eranmanim@gmail.com>
2026-03-12 12:34:54 +0200

    feat: add config file to configure required env variables

---
+++ app/core/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    """
+    BaseSettings loads configuration from environment variables, validating them as pydantic models
+    Provide default values
+    Easier to declare and validate rather then using os.getenv() directly
+    Removes the need of using python-dotenv as well
+    A single source of truth
+
+    NOTE: By default, Pydantic maps attribute names to env vars (case-insensitive)
+    e.g database_url ->	DATABASE_URL
+    """
+    database_url: str
+    redis_url: str
+    secret_key: str
+    access_token_expire_minutes: int = 60
+
+    # Load variables from .env automatically
+    # Ignore extra env variables not defined in the model
+    model_confg = {
+        "env_file": ".env",
+        "extra": "ignore"
+    }
+
+settings = Settings()
