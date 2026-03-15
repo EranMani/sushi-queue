753a4a55244381a8d511799a70f237af78308580
EranMani <eranmanim@gmail.com>
2026-03-15 08:28:51 +0200

    refactor: expose the OrderStatus enum as a public entry for the order schema

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.models.user import User
 from app.models.menu_item import MenuItem
-from app.models.order import Order
+from app.models.order import Order, OrderStatus
 from app.models.order_item import OrderItem
 
-__all__ = ["User", "MenuItem", "Order", "OrderItem"]
+__all__ = ["User", "MenuItem", "Order", "OrderItem", "OrderStatus"]
